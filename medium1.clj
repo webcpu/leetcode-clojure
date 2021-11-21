79f9f2d5465ddaf015a285a613ead1a1ba4dd2b3
liang <liang@unchartedworks.com>
2021-11-21 16:57:05 +0100

    #74

--- medium1.clj
+++ medium1.clj
@@ -979,4 +979,16 @@
                 :else (search (inc r) c)
                 ))]
       (search 0 (dec n)))))
+(defn search-matrix [matrix target]
+  (let [m (count matrix)
+        n (count (first matrix))]
+    (letfn [(search [left right]
+              (let [mid (quot (+ left right) 2)
+                    num ((matrix (quot mid m)) (rem mid n))]
+                (cond
+                  (> left right) false
+                  (< target num) (search left (dec mid))
+                  (> target num) (search (inc mid) right)
+                  :else true)))]
+    (search 0 (dec (* m n))))))
 (map (partial apply search-matrix) ['([[1,3,5,7],[10,11,16,20],[23,30,34,60]] 3) '([[1,3,5,7],[10,11,16,20],[23,30,34,60]] 13)])
